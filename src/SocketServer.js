cd89f55b0c89221e856e39e2c5c3c30ab9ea8e88
Oleg Grunski <oleggr578@gmail.com>
2024-09-02 23:14:38 +0200

    create send-message socket

--- src/SocketServer.js
+++ src/SocketServer.js
@@ -2,4 +2,15 @@ export default function (socket, io) {
 	socket.on('newPainting', (newPainting) => {
         io.emit('get-paintings', newPainting)
     })
+
+	socket.on('newProduct', (newProduct) => {
+        const userId = newProduct.userId;
+        io.emit(`get-products/${userId}`, newProduct)
+    })
+
+	socket.on('send-message', (newMessage) => {
+        const userId = newMessage.userId;
+        const authorId = newMessage.authorId;
+        io.emit(`get-messages-by-user-and-author-id/${userId}/${authorId}`, newMessage)
+    })
 }
