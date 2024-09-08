4378dae5e69bf79f4ad845ca5959d09a2d82d28d
Oleg Grunski <oleggr578@gmail.com>
2024-09-08 12:04:48 +0200

    added update unread messages route

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -11,7 +11,9 @@ import {
 	resetPassword,
 	verifyEmail,
 	getUsers,
-	get_author_by_id
+	get_author_by_id,
+	refreshUser,
+	updateUnreadMessages
 } from '../controllers/auth.controller.js';
 import { authMiddleware } from '../middleware/auth.middleware.js';
 
@@ -19,12 +21,15 @@ const router = express.Router();
 
 router.route('/get-author-by-id/:authorId').get(trimRequest.all, get_author_by_id);
 router.route('/register').post(trimRequest.all, register);
+router.route('/refresh-user-by-id/:userId').get(trimRequest.all, refreshUser);
 router.route('/login').post(trimRequest.all, login);
 router.route('/logout').post(trimRequest.all, logout);
 router.route('/forgot-password').post(trimRequest.all, forgotPassword);
 router.route('/reset-password').post(trimRequest.all, resetPassword);
 router.route('/verify-email').get(trimRequest.all, verifyEmail);
 router.route('/get-users').get(trimRequest.all, getUsers);
+
+router.route('/update-unread-messages/:userId/:senderId').put(trimRequest.all, updateUnreadMessages);
 
 router.route('/refreshtoken').post(trimRequest.all, refreshToken);
 router.route('/testing-auth-middleware').get(trimRequest.all, authMiddleware, testAuthMiddleware);
