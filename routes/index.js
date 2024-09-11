14980a5d5ad30b9ab7b4f9022675efc94646ca17
Oleg Grunski <oleggr578@gmail.com>
2024-09-11 20:16:32 +0200

    connected message route  into index backend

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ import express from "express";
 
 import authRouter from "./auth.route.js";
 import paintingRouter from './painting.route.js'
+import productRouter from './product.route.js'
+import messageRouter from './message.route.js'
 
 const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/painting", paintingRouter);
+router.use("/cart", productRouter);
+router.use("/message", messageRouter);
 
 export default router
