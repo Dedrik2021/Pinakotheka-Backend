9b5f0983476060c91279744ec96160702fb83863
Oleg Grunski <oleggr578@gmail.com>
2024-10-07 21:12:09 +0200

    created update and delete message routes

--- routes/message.route.js
+++ routes/message.route.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import trimRequest from 'trim-request'
 
-import { sendMessage, getMessagesByUserId, getMessagesByUserIdAndAuthorId } from '../controllers/message.controller.js'
+import { sendMessage, getMessagesByUserId, getMessagesByUserIdAndAuthorId, updateAndRemoveDeletedMessageById, deleteConversationByUserIdAndAuthorId } from '../controllers/message.controller.js'
 
 const router = express.Router()
 
 router.route('/create-message').post(trimRequest.all, sendMessage)
 router.route('/get-messages-by-user-id/:userId').get(trimRequest.all, getMessagesByUserId)
 router.route('/get-messages-by-user-and-author-id/:userId/:authorId').get(trimRequest.all, getMessagesByUserIdAndAuthorId)
+router.route('/update-and-remove-deleted-message-by-id/:messageId').put(trimRequest.all, updateAndRemoveDeletedMessageById)
+router.route('/delete-conversation/:userId/:authorId').put(trimRequest.all, deleteConversationByUserIdAndAuthorId)
 
 export default router
